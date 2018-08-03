d9da90c122311ed9b5c25545caa088dc1d5272b9
saguywalker <guyguy252@gmail.com>
2018-08-03 16:55:50 +0700

    add download_data() function

--- eeg.py
+++ eeg.py
@@ -1,8 +1,22 @@
 import requests
-url_start = 'https://www.physionet.org/pn4/eegmmidb/S025/S025R'
-url_end = '.edf'
-file_start = 'S025R'
-for i in range(1,15):
-    res = requests.get(url_start+str(i).zfill(2)+url_end)
-    with open(file_start+str(i).zfill(2)+url_end,'wb') as code:
-        code.write(res.content)
+import os
+from six.moves import urllib
+
+download_root = 'https://www.physionet.org/pn4/eegmmidb/'
+file_end = '.edf'
+
+def download_data():
+    if not os.path.isdir("subjects"):
+        os.makedirs("subjects")
+    for j in range(1,110):
+        folder_name = "S"+str(j).zfill(3)
+        current_path = os.path.join("subjects",folder_name)
+        if not os.path.isdir(current_path):
+            os.makedirs(current_path)
+            print(current_path,"is created")
+        for i in range(1,15):
+            file_name = folder_name+"R"+str(i).zfill(2)+file_end
+            res = requests.get(download_root+"/"+folder_name+file_name)
+            with open("./subjects/"+folder_name+"/"+file_name,'wb') as w:
+                w.write(res.content)
+        print("Subject:",j,"data is downloaded")

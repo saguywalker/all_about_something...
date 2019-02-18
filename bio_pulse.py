d21aa83fe7b3136038c9c1a2fafdd2595ad02eb3
saguywalker <guyguy252@gmail.com>
2019-02-19 00:11:08 +0700

    Change to run with many folders.

--- bio_pulse.py
+++ bio_pulse.py
@@ -1,3 +1,5 @@
+#!/usr/bin/ python3
+
 import os
 import pandas as pd
 import numpy as np
@@ -6,26 +8,37 @@ import sys
 import datetime
 import re
 
-pattern_time = re.compile('\d+:\d+:\d+.\d+')
-pattern_date = re.compile('\d+-\d+-\d+')
+P_TIME = re.compile('\d+:\d+:\d+.\d+')
+P_DATE = re.compile('\d+-\d+-\d+')
 
 def get_data(file):
     path = os.path.join(file[0], file[1])
     data = pd.read_csv(path)
-    if data.shape[0] != 6:
+
+    for col in data:
+        if "sampling rate" in col:
+            srate = float(col[-3:])
+        if "time" in col:
+            time = P_TIME.search(col).group()
+        if "date" in col:
+            date = P_DATE.search(col).group()
+    data = data.values[1:, 4]
+    timestamp = datetime.datetime.strptime(date+" "+time, "%Y-%m-%d %H:%M:%S.%f")
+
+    """if data.shape[0] != 6:
         for col in data:
             if "sampling rate" in col:
                 srate = float(col[-3:])
             elif "time" in col:
-                time = pattern_time.search(col).group()
+                time = P_TIME.search(col).group()
             elif "date" in col:
-                date = pattern_date.search(col).group()
+                date = P_DATE.search(col).group()
         data = data.values[1:, 4]
         timestamp = datetime.datetime.strptime(date+" "+time, "%Y-%m-%d %H:%M:%S.%f")
     else:
         srate = 300
         timestamp = datetime.datetime.now()
-        data = data.values[:, 4]
+        data = data.values[:, 4]"""
 
     return (timestamp, srate, data.astype(np.float32))
 
@@ -43,14 +56,26 @@ def get_hr(data):
         timestamp = data[0] + datetime.timedelta(seconds=time)
         ans = "{:.2f}".format(60/duration)
         hr.append((timestamp.strftime("%Y-%m-%d %H:%M:%S.%f")[:-1], ans))
+    print("{} hr(s) was found from {} events, ({},{},{})\n".format(len(hr), events.shape, data[0], data[1], data[2].shape))
     return (hr, times[-1], avg_pulse)
 
-def write_output(fname, result):
-    df = pd.DataFrame(result[0], columns=["timestamp", "heart rate"])
+def write_output(fname, results):
+    all_data = results[0][0]
+    print("all_data:",len(all_data), len(all_data[0]))
+    print("len: {}".format(len(results)))
+    if len(results) > 1:
+        for r in results[1:]:
+            print("data shape: {}".format(len(r[0])))
+            if len(r[0]) > 0:
+                all_data = np.concatenate((all_data, r[0]), axis=0)
+                print(all_data.shape)
+
+    df = pd.DataFrame(all_data, columns=["timestamp", "heart rate"])
+    df = df.sort_values(by="timestamp")
     df.to_csv(os.path.join("heartrate",fname+"-result.csv"), index=False, na_rep=None)
 
-if __name__ == "__main__":
-    file_path = os.path.join(sys.argv[1], "Biosignalsplux")
+def read_each_folder(folder, sub_folder):
+    file_path = os.path.join(folder, sub_folder)
     files = [(file_path, f) for f in os.listdir(file_path) if ".csv" in f]
     print("List of files...")
     for file in files:
@@ -58,5 +83,13 @@ if __name__ == "__main__":
     data = [get_data(file) for file in files]
     hr = [get_hr(d) for d in data]
 
-    for i, h in enumerate(hr):
-        write_output(files[i][1], h)
+    write_output(folder, hr)
+    print("{} is writed".format(folder))
+    print("*"*30)
+
+if __name__ == "__main__":
+    folders = [f for f in os.listdir() if "Subject09" in f]
+    folders.sort()
+    #folders = [sys.argv[1]]
+    for folder in folders:
+        read_each_folder(folder, "Biosignalsplux")
